a5c0ef5790b77375c1d5e4255ddf970c5ce79833
OmairKhugyani <115149701+OmairKhugyani@users.noreply.github.com>
2023-10-02 21:09:54 +0200

    Add files via upload

---
+++ BookLibValidatorUnitTestTests/BookRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+[TestClass]
+public class BooksRepositoryTests
+{
+    [TestMethod]
+    public void Get_ReturnsAllBooks()
+    {
+        // Arrange
+        var repository = new BooksRepository();
+
+        // Act
+        var result = repository.Get();
+
+        // Assert
+        Assert.AreEqual(5, result.Count);
+    }
+
+    [TestMethod]
+    public void Get_FilterByPrice_ReturnsFilteredBooks()
+    {
+        // Arrange
+        var repository = new BooksRepository();
+
+        // Act
+        var result = repository.Get(150);
+
+        // Assert
+        Assert.AreEqual(3, result.Count);
+        Assert.IsTrue(result.All(book => book.Price <= 150));
+    }
+
+    [TestMethod]
+    public void GetById_ValidId_ReturnsCorrectBook()
+    {
+        // Arrange
+        var repository = new BooksRepository();
+        int validId = 3;
+
+        // Act
+        var result = repository.GetById(validId);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(validId, result.Id);
+    }
+}
