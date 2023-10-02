5c13b4a183daa7afbe5fe81ef77d178469e917bf
OmairKhugyani <115149701+OmairKhugyani@users.noreply.github.com>
2023-10-02 21:08:21 +0200

    Add files via upload

---
+++ BookLibValidatorUnitTest/Book.cs
@@ -0,0 +1,39 @@
+﻿namespace BookLibValidatorUnitTest;
+using System;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Title: {Title}, Price: {Price:C}";
+    }
+
+    public void ValidateId()
+    {
+        if (Id < 1)
+            throw new ArgumentException("Id must be a positive integer.");
+    }
+
+    public void ValidateTitle()
+    {
+        if (string.IsNullOrEmpty(Title) || Title.Length < 3)
+            throw new ArgumentException("Title must be at least 3 characters long and cannot be null or empty.");
+    }
+
+    public void ValidatePrice()
+    {
+        if (Price <= 0 || Price > 1200)
+            throw new ArgumentException("Pris skal være i mellem 1 og 1200.");
+    }
+
+    public void ValidateBook()
+    {
+        ValidateId();
+        ValidateTitle();
+        ValidatePrice();
+    }
+}
---
+++ BookLibValidatorUnitTest/BookRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using BookLibValidatorUnitTest;
+
+public class BooksRepository
+{
+    private List<Book> books;
+
+    public BooksRepository()
+    {
+        // Opret en liste af bogobjekter
+        books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Price = 100 },
+            new Book { Id = 2, Title = "Book 2", Price = 200 },
+            new Book { Id = 3, Title = "Book 3", Price = 150 },
+            new Book { Id = 4, Title = "Book 4", Price = 300 },
+            new Book { Id = 5, Title = "Book 5", Price = 50 }
+        };
+    }
+
+    public List<Book> Get()
+    {
+        // Returner en kopi af listen ved hjælp af en copy constructor
+        return new List<Book>(books);
+    }
+
+    public List<Book> Get(decimal maxPrice)
+    {
+        // Returner en kopi af listen filtreret efter pris
+        return new List<Book>(books.Where(book => book.Price <= maxPrice));
+    }
+
+    public List<Book> Get(string sortBy)
+    {
+        // Returner en kopi af listen sorteret efter titel eller pris
+        if (sortBy.Equals("Title", StringComparison.OrdinalIgnoreCase))
+        {
+            return new List<Book>(books.OrderBy(book => book.Title));
+        }
+        else if (sortBy.Equals("Price", StringComparison.OrdinalIgnoreCase))
+        {
+            return new List<Book>(books.OrderBy(book => book.Price));
+        }
+        else
+        {
+            throw new ArgumentException("Invalid sort option. Use 'Title' or 'Price'.");
+        }
+    }
+
+    public Book GetById(int id)
+    {
+        // Find og returner bogen med det angivne id, eller returner null
+        return books.FirstOrDefault(book => book.Id == id);
+    }
+
+    public Book Add(Book book)
+    {
+        // Tildel et nyt id til bogen og tilføj den til listen
+        int newId = books.Max(b => b.Id) + 1;
+        book.Id = newId;
+        books.Add(book);
+        return book;
+    }
+
+    public Book Delete(int id)
+    {
+        // Find og fjern bogen med det angivne id fra listen, eller returner null
+        Book bookToRemove = books.FirstOrDefault(book => book.Id == id);
+        if (bookToRemove != null)
+        {
+            books.Remove(bookToRemove);
+        }
+        return bookToRemove;
+    }
+
+    public Book Update(int id, Book values)
+    {
+        // Find bogen med det angivne id og opdater dens egenskaber med values
+        Book bookToUpdate = books.FirstOrDefault(book => book.Id == id);
+        if (bookToUpdate != null)
+        {
+            bookToUpdate.Title = values.Title;
+            bookToUpdate.Price = values.Price;
+        }
+        return bookToUpdate;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
